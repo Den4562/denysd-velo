f344760d76eb5db8368c2730c070cff7b96e6c8e
Den4562 <119700990+Den4562@users.noreply.github.com>
2025-05-24 20:04:35 +0300

    add data.js

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,13 +1,1 @@
-import wixData from "wix-data";
-
-export function Employees_beforeInsert(item, context) {
-  item.salary = item.hoursPerMonth * item.hourlyRate;
-  return item;
-}
-
-// Хук перед оновленням запису
-export function Employees_beforeUpdate(item, context) {
-  item.salary = item.hoursPerMonth * item.hourlyRate;
-  return item;
-}
 $w.onReady(function () {});
