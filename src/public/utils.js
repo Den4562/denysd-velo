bf9bc222ce8f19e64a65c4ffb8d432192fc75de2
Den4562 <119700990+Den4562@users.noreply.github.com>
2025-05-25 08:10:17 +0300

    add event and utils

---
+++ src/public/utils.js
@@ -0,0 +1,9 @@
+export function clearInputs() {
+  $w("#inpFirstName").value = "";
+  $w("#inpLastName").value = "";
+  $w("#inpAge").value = "";
+  $w("#inpPosition").value = "";
+  $w("#inpHoursPerMonth").value = "";
+  $w("#inpHourlyRate").value = "";
+  $w("#inpHireDate").value = null;
+}
