c2595e67af8bc567ec5fb360af7151f335294251
Den4562 <119700990+Den4562@users.noreply.github.com>
2025-05-25 08:34:40 +0300

    update

--- src/public/utils.js
+++
@@ -1,9 +0,0 @@
-export function clearInputs() {
-  $w("#inpFirstName").value = "";
-  $w("#inpLastName").value = "";
-  $w("#inpAge").value = "";
-  $w("#inpPosition").value = "";
-  $w("#inpHoursPerMonth").value = "";
-  $w("#inpHourlyRate").value = "";
-  $w("#inpHireDate").value = null;
-}
